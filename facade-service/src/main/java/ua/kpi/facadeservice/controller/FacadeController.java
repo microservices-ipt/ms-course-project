b0b1f15cc84493c7d52de2d2890e13b4e0c24f76
Pozzitron1337 <the.vaho1337@gmail.com>
2022-05-09 02:24:04 +0300

    separate service and controller of Facade and hardcoded three instances of logging services

--- facade-service/src/main/java/ua/kpi/facadeservice/controller/FacadeController.java
+++ facade-service/src/main/java/ua/kpi/facadeservice/controller/FacadeController.java
@@ -1,55 +1,35 @@
 package ua.kpi.facadeservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-import ua.kpi.sharedmodel.Message;
+import ua.kpi.facadeservice.service.FacadeService;
 
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/facade")
 @Slf4j
 public class FacadeController {
 
-    WebClient loggingWebClient = WebClient.create("http://localhost:8081");
-    WebClient messagesWebClient = WebClient.create("http://localhost:8082");
+    public FacadeService facadeService;
+
+    public FacadeController(FacadeService facadeService) {
+        this.facadeService = facadeService;
+    }
+
 
     @GetMapping("/hello")
     public String helloWorld() {
-        return "world!";
+        return facadeService.helloWorld();
     }
 
     @GetMapping()
     public String getMessages() {
-        var loggingResponse = loggingWebClient
-                .get()
-                .uri("/logging")
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
-        var messageResponse = messagesWebClient
-                .get()
-                .uri("/message")
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
-        return loggingResponse + " : " + messageResponse;
+        return facadeService.getMessages();
     }
 
     @PostMapping()
     public void writeMessage(@RequestBody String body) {
-        var msg = new Message(UUID.randomUUID(), body);
-        log.info("Message {}", msg);
-        loggingWebClient.post()
-                .uri("/logging")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(Mono.just(msg), Message.class)
-                .retrieve()
-                .bodyToMono(Void.class)
-                .block();
+        facadeService.writeMessage(body);
     }
 
 }
---
+++ facade-service/src/main/java/ua/kpi/facadeservice/service/FacadeService.java
@@ -0,0 +1,70 @@
+package ua.kpi.facadeservice.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import ua.kpi.sharedmodel.Message;
+
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Service
+@Slf4j
+public class FacadeService {
+
+    public List<WebClient> loggingWebClients;
+    public WebClient messagesWebClient;
+
+    public FacadeService() {
+        loggingWebClients = List.of(
+                WebClient.create("http://localhost:8081"),
+                WebClient.create("http://localhost:8083"),
+                WebClient.create("http://localhost:8084")
+        );
+        messagesWebClient = WebClient.create("http://localhost:8082");
+    }
+
+    public String helloWorld() {
+        return "world!";
+    }
+
+    public String getMessages() {
+        int randomNumber = new Random().nextInt(loggingWebClients.size());
+        log.info("random number: {}", randomNumber);
+        var loggingWebClient = loggingWebClients.get(randomNumber);
+        var loggingResponse = loggingWebClient
+                .get()
+                .uri("/logging")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        var messageResponse = messagesWebClient
+                .get()
+                .uri("/message")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        return loggingResponse + " : " + messageResponse;
+    }
+
+    public void writeMessage(@RequestBody String body) {
+        int randomNumber = new Random().nextInt(loggingWebClients.size());
+        log.info("random number: {}", randomNumber);
+        var msg = new Message(UUID.randomUUID(), body);
+        log.info("Message {}", msg);
+        var loggingWebClient = loggingWebClients.get(randomNumber);
+        loggingWebClient.post()
+                .uri("/logging")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(msg), Message.class)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .block();
+    }
+
+}
